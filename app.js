ac10db1c3fec38127d112f223d609b734680f434
Andrew Salcedo <asalcedo07@gmail.com>
2017-06-03 20:39:12 -0400

    refactor: url helper(), add: getPosts() method.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const NYTBaseUrl = 'https://api.nytimes.com/svc/topstories/v2/'
 const apiKey = '7d61a6720f1c464dbb2473ebb4823011'; 
 
+function buildUrl (url) {
+    return NYTBaseUrl + url + ".json?api-key=" + apiKey 
+}
 
 const vm = new Vue ({
     el: "#app", 
@@ -7,8 +11,17 @@ const vm = new Vue ({
         results: []
     }, 
      mounted() {
-        axios.get("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=7d61a6720f1c464dbb2473ebb4823011")
-        .then(response => {this.results = response.data.results})
-  }
+        // axios.get("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=7d61a6720f1c464dbb2473ebb4823011")
+        // .then(response => {this.results = response.data.results})
+        this.getPosts('home'); 
+    }, 
+    methods: {
+        getPosts(section) {
+            let url = buildUrl(section); 
+            axios.get(url).then((response) => {
+                this.results = response.data.results; 
+            }).catch( error => { console.log(error); }); 
+        }
+    }
 }); 
-     
+    
